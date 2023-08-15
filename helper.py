3876a67e0b04bc8f6277e1193d1d46828a35d6ad
monson2002 <monsonreji2002@gmail.com>
2023-08-15 20:11:06 +0530

    8:10pm 15-8-23

--- helper.py
+++ helper.py
@@ -57,20 +57,93 @@ def no_of_atheletes(df):
 def no_of_participating_nations(df):
     return len(df["Region"].unique().tolist())
 
+
 def nations_over_the_years(df):
-    YOY_country_participation_data = df.drop_duplicates(subset=["Year","Region"])["Year"].value_counts().reset_index().sort_values(by="Year")
-    YOY_country_participation_data.rename(columns={"count":"Number of Participating Countries"}, inplace=True)
+    YOY_country_participation_data = df.drop_duplicates(subset=["Year", "Region"])[
+        "Year"].value_counts().reset_index().sort_values(by="Year")
+    YOY_country_participation_data.rename(
+        columns={"count": "Number of Participating Countries"}, inplace=True)
 
     return YOY_country_participation_data
 
+
 def events_over_the_years(df):
-    YOY_events_data = df.drop_duplicates(subset=["Year","Event"])["Year"].value_counts().reset_index().sort_values(by="Year")
-    YOY_events_data.rename(columns={"count":"Number of Events"}, inplace=True)
+    YOY_events_data = df.drop_duplicates(subset=["Year", "Event"])[
+        "Year"].value_counts().reset_index().sort_values(by="Year")
+    YOY_events_data.rename(columns={"count": "Number of Events"}, inplace=True)
 
     return YOY_events_data
 
+
 def athletes_over_the_years(df):
-    YOY_atheletes_data = df.drop_duplicates(subset=["Year","Name"])["Year"].value_counts().reset_index().sort_values(by="Year")
-    YOY_atheletes_data.rename(columns={"count":"Number of Athletes"}, inplace=True)
+    YOY_atheletes_data = df.drop_duplicates(subset=["Year", "Name"])[
+        "Year"].value_counts().reset_index().sort_values(by="Year")
+    YOY_atheletes_data.rename(
+        columns={"count": "Number of Athletes"}, inplace=True)
 
     return YOY_atheletes_data
+
+
+def top_15_countries(df):
+    df.drop_duplicates(subset=["NOC", "Games", "Year", "Season",
+                       "City", "Sport", "Event", "Medal", "Region"], inplace=True)
+    dic = {}
+    df.dropna(inplace=True)
+    df.groupby("Region").count()["Medal"].sort_values(ascending=False)
+    for i in df["Region"].unique():
+        temp_df = df[(df['Region'] == i)]
+        dic[i] = temp_df["Medal"].count()
+    l = sorted(dic.items(), key=lambda x: x[1], reverse=True)[:15]
+    labels = []
+    values = []
+    for i in l:
+        labels.append(i[0])
+        values.append(i[1])
+    return labels, values
+
+
+def country_analysis_dropdown(df, country):
+    country_NOC = df[df["Region"] == country]["NOC"].unique()[0]
+    df.drop_duplicates(subset=["NOC", "Games", "Year", "Season",
+                       "City", "Sport", "Event", "Medal", "Region"], inplace=True)
+    temp_df = df[df["NOC"] == country_NOC]
+    final_df = temp_df.groupby("Year").count()["Medal"].reset_index()
+    return final_df
+
+
+def top_15_per_country(df, country):
+    temp_df = df[df["Region"] == country]
+    temp_df.drop_duplicates(subset=["Team","NOC","Games","Year","Season","City","Sport","Event","Medal","Region"], inplace=True)
+    temp_df[temp_df["Sport"] == "Hockey"]
+    final_df = temp_df.groupby(by=["Name"])[["Gold","Silver","Bronze"]].sum().sort_values(by=["Gold","Silver","Bronze"],ascending=False).reset_index()
+    final_df["Total"] = final_df["Gold"] + final_df["Silver"] + final_df["Bronze"] 
+    return final_df.head(15)
+
+
+def athelete_tally_age_gold_silver_bronze(df):
+    athlete_df = df.drop_duplicates(subset=["Name","Sex","Region"])
+    gold = athlete_df[athlete_df["Medal"] == "Gold"]["Age"].dropna()
+    silver = athlete_df[athlete_df["Medal"] == "Silver"]["Age"].dropna()
+    bronze = athlete_df[athlete_df["Medal"] == "Bronze"]["Age"].dropna()
+    l = [gold, silver, bronze]
+    return l
+
+def athelete_age_vs_sport(df):
+    athlete_df = df.drop_duplicates(subset=["Name","Sex","Region"])
+    x = []
+    name = []
+    famous_sports = ['Basketball', 'Judo', 'Football', 'Tug-Of-War', 'Athletics',
+                     'Swimming', 'Badminton', 'Sailing', 'Gymnastics',
+                     'Art Competitions', 'Handball', 'Weightlifting', 'Wrestling',
+                     'Water Polo', 'Hockey', 'Rowing', 'Fencing',
+                     'Shooting', 'Boxing', 'Taekwondo', 'Cycling', 'Diving', 'Canoeing',
+                     'Tennis', 'Golf', 'Softball', 'Archery',
+                     'Volleyball', 'Synchronized Swimming', 'Table Tennis', 'Baseball',
+                     'Rhythmic Gymnastics', 'Rugby Sevens',
+                     'Beach Volleyball', 'Triathlon', 'Rugby', 'Polo', 'Ice Hockey']
+    for sport in famous_sports:
+        temp_df = athlete_df[athlete_df['Sport'] == sport]
+        x.append(temp_df[temp_df['Medal'] == 'Gold']['Age'].dropna())
+        name.append(sport)
+        
+    return x, name
--- index.py
+++ index.py
@@ -3,6 +3,9 @@ import pandas as pd
 from Analysis import preprocess
 import helper
 import plotly.express as px
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("./Datasets/athlete_events.csv")
 df_regions = pd.read_csv("./Datasets/noc_regions.csv")
@@ -13,21 +16,22 @@ st.sidebar.title("Olympics Analysis")
 
 clicked_option = st.sidebar.radio(
     "Select an option:",
-    ("Medal Tally", "Overall Analysis", "Country-wise Analysis", "Athlete-wise Analysis")
-)
-
-st.sidebar.header("Filters")
-
-dropdown_year = st.sidebar.selectbox(
-    "Select Year", helper.year_dropdown(df)
-)
-
-dropdown_country = st.sidebar.selectbox(
-    "Select Country", helper.country_dropdown(df)
+    ("Medal Tally", "Overall Analysis",
+     "Country-wise Analysis", "Athlete-wise Analysis")
 )
 
 
 if clicked_option == "Medal Tally":
+    st.sidebar.header("Filters")
+
+    dropdown_year = st.sidebar.selectbox(
+        "Select Year", helper.year_dropdown(df)
+    )
+
+    dropdown_country = st.sidebar.selectbox(
+        "Select Country", helper.country_dropdown(df)
+    )
+
     if dropdown_country == "Region" and dropdown_year == "Year":
         st.header("Overall Medal Tally")
     elif dropdown_country != "Region" and dropdown_year == "Year":
@@ -42,7 +46,7 @@ if clicked_option == "Medal Tally":
 
 if clicked_option == "Overall Analysis":
     st.header("Analysis of Overall Games")
-    
+
     col1, col2, col3 = st.columns(3)
     x = helper.no_of_editions(df)
     with col1:
@@ -58,7 +62,7 @@ if clicked_option == "Overall Analysis":
     with col3:
         st.subheader("Events: ")
         st.title(x)
-    
+
     col1, col2 = st.columns(2)
     x = helper.no_of_atheletes(df)
     with col1:
@@ -69,25 +73,58 @@ if clicked_option == "Overall Analysis":
     with col2:
         st.subheader("Nations: ")
         st.title(x)
-        
-    
+
     st.header("Participating Nations over the years")
     nations_over_the_years = helper.nations_over_the_years(df)
-    fig = px.line(nations_over_the_years, x="Year", y="Number of Participating Countries") 
-    st.plotly_chart(fig) 
+    fig = px.line(nations_over_the_years, x="Year",
+                  y="Number of Participating Countries")
+    st.plotly_chart(fig)
 
     st.header("Number of Events over the years")
     events_over_the_years = helper.events_over_the_years(df)
-    fig = px.line(events_over_the_years, x="Year", y="Number of Events") 
+    fig = px.line(events_over_the_years, x="Year", y="Number of Events")
     st.plotly_chart(fig)
-    
+
     st.header("Number of Athletes over the years")
     athletes_over_the_years = helper.athletes_over_the_years(df)
-    fig = px.line(athletes_over_the_years, x="Year", y="Number of Athletes") 
-    st.plotly_chart(fig) 
-    
+    fig = px.line(athletes_over_the_years, x="Year", y="Number of Athletes")
+    st.plotly_chart(fig)
 
-     
-    
-    
 
+if clicked_option == "Country-wise Analysis":
+    st.header("Top 15 Countries")
+    labels, values = helper.top_15_countries(df)
+    fig = go.Figure(data=[go.Pie(labels=labels,
+                                 values=values,
+                                 textinfo='label+percent',
+                                 insidetextorientation='radial'
+                                 )])
+    st.plotly_chart(fig)
+
+    st.header("Analysis based on Country")
+    dropdown_country = st.selectbox(
+        "Select Country", helper.country_dropdown(df)
+    )
+    if dropdown_country != "Region":
+        fig = px.line(helper.country_analysis_dropdown(
+            df, dropdown_country), x="Year", y="Medal")
+        st.plotly_chart(fig)
+
+        st.header("Top 15 Athletes of " + dropdown_country)
+        st.table(helper.top_15_per_country(df, dropdown_country))
+
+
+if clicked_option == "Athlete-wise Analysis":
+    st.header("Athelete Analysis Based On Age")
+    gold, silver, bronze = helper.athelete_tally_age_gold_silver_bronze(
+        df)
+    fig = ff.create_distplot([gold, silver, bronze], [
+                             "Golds", "Silver", "Bronze"], show_hist=False, show_rug=False)
+    st.plotly_chart(fig)
+    
+    st.header("Athelete Analysis Based On Age and Sports")
+    x, name = helper.athelete_age_vs_sport(df)    
+    plt.figure(figsize=(1, 1))
+    fig = ff.create_distplot(x, name, show_hist=False, show_rug=False)
+    fig.update_layout(autosize=False, width=800, height=600)
+    st.plotly_chart(fig)
